abed1961eead63a4b31759477a45e99a6efb9382
raimundoneto01 <raimundogomes826@gmail.com>
2024-06-03 20:26:51 -0300

    conexão pra render

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import Table from "./components/table/Table";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getProdutos = async () => {
+    const URL_PRODUTOS = "https://dripstore-api-2ych.onrender.com/api/produto";
+
+    try {
+      const resulte = await axios.get(URL_PRODUTOS);
+      console.log(resulte);
+    } catch (error) {
+      console.log(`Erro ao logar${error} com async`);
+    }
+  };
+
+  useEffect(() => {
+    getProdutos();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Table />
+    </div>
+  );
 }
 
-export default App
+export default App;
